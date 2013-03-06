0f929de15b8a5f66960983763b9f61e76f8de699
dennis.knebel <dennis.knebel@education-siemens.com>
2013-03-06 12:53:45 +0100

    output ready

--- source files/output.c
+++ source files/output.c
@@ -3,5 +3,10 @@
 void fctOutput(float fRes, float fCoi, float fCap, float fLed)
 {
    //suggestion:
-//   printf("\nPrice for resistors:\t %.2f",fRes);
+printf("\nPrice for resistors:\t %.2f",fRes);
+
+printf("\nPrice for coils:\t %.2f",fCoi);
+printf("\nPrice for capacitors:\t %.2f",fCap);
+printf("\nPrice for LEDs:\t %.2f",fLed);
+
 }
